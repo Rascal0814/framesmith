1a9fca0c62b5b419d053e083581bd8ea1070817c
root <root@DESKTOP-M0AUQ52.>
2026-03-09 13:17:14 +0800

    fix: 修复路由basename和资源路径

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/framesmith/">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/video/:id" element={<VideoDetail />} />
